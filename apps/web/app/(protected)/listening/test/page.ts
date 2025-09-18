7c5e9a8e85a9a549cfc5d29f16273dc48ce2f2d7
seaneda-manager <seaneda@gmail.com>
2025-09-18 14:42:19 +0900

    Remove leftover .ts duplicates (use .tsx only)

--- apps/web/app/(protected)/listening/test/page.ts
+++
@@ -1,7 +0,0 @@
-import Player from './Player'
-
-export default function Page() {
-  const audioSrc = '/media/p1.mp3'
-  const sessionId = '...서버에서 만든 세션 ID...'
-  return <Player audioSrc={audioSrc} sessionId={sessionId} />
-}
